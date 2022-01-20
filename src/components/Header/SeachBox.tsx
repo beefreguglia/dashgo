6b5eab0a0edd0c42de46afe37faac9ff15a135b8
beefreguglia <beefreguglia@gmail.com>
2022-01-20 00:01:07 -0300

    Adicionando react-hook-form ao projeto

--- src/components/Header/SeachBox.tsx
+++ src/components/Header/SeachBox.tsx
@@ -1,7 +1,29 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
+import { useRef } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
+// Temos 2 formas de tratar formularios no React
+
+// Controlled components =>
+//   São aqueles que nós criamos um estado com o conteúdo
+//   iniciando com o valor vazio passando no input o
+//   value={VariavelEstado} e o 
+//   onChange={event => setVariavelEstado(event.target.value)}
+//   assim recebendo o valor do input.
+//   Para Utilizarmos logicas de escrever algo no input e
+//   fazer algo devemos utilizar a lógica de bebounce.
+
+// Uncontrolled components => 
+//    São formas de acessar o valor do input só no momento que
+//    precisarmos dele nao precisando armazena-lo em um estado
+//    acessando ele só quando necessario utilizando uma
+//    const variavel = useRef<TipoElemento(HTMLInputElement)>(null);
+//    usando a propriedade ref={variavel} no componente de modo
+//    imperativo
+
 export function SearchBox() {
+  const searchInputRef = useRef<HTMLInputElement>();
+  
   return(
     <Flex
         as="label"
@@ -24,6 +46,7 @@ export function SearchBox() {
           _placeholder={{
             color: 'gray.400'
           }}
+          ref={searchInputRef}
         />
         <Icon as={RiSearchLine} fontSize="20" />
       </Flex>
