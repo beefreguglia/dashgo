53c37b07fdcaddb3bda5b0afa14044cb89179d5c
beefreguglia <beefreguglia@gmail.com>
2022-02-02 00:13:47 -0300

    Adicionando axios, miragejs e fazendo conexao com apis

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,42 +16,20 @@ import {
   useBreakpointValue,
 } from '@chakra-ui/react';
 import Link from 'next/link';
-import { useEffect } from 'react';
 import { RiAddBoxLine, RiPencilLine } from 'react-icons/ri';
-import { useQuery } from 'react-query';
 
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
-    
-    const users = data.users.map(user => {
-      return{
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    });
-    return users;
-  },
-  {
-    staleTime: 1000 * 5 //5 segundos
-  });
-
+  const { data, isLoading, isFetching, error } = useUsers();
+  
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
   return (
     <Box>
       <Header />
@@ -61,6 +39,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading sg="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
@@ -96,39 +77,45 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                 {data.map(user => {
-                   return(
-                    <Tr key={user.id}>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">{user.name}</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          {user.email}
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>{user.createdAt}</Td>}
-                    <Td>
-                      {isWideVersion && (
-                        <Button
-                          as="a"
-                          size="sm"
-                          colorScheme="purple"
-                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                        >
-                          Editar
-                        </Button>
-                      )}
-                    </Td>
-                  </Tr>
-                   )
-                 })}
+                  {data.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        <Td>
+                          {isWideVersion && (
+                            <Button
+                              as="a"
+                              size="sm"
+                              colorScheme="purple"
+                              leftIcon={
+                                <Icon as={RiPencilLine} fontSize="16" />
+                              }
+                            >
+                              Editar
+                            </Button>
+                          )}
+                        </Td>
+                      </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination 
+                totalCountOfRegisters={200}
+                currentPage={4}
+                onPageChange={() => {}}
+              />
             </>
           )}
         </Box>
